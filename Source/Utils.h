56ab5ffaa34c8113f7c931d35d5d9f2acc15786a
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2025-04-03 15:42:54 -0700

    Auto push after build

--- Source/Utils.h
+++ Source/Utils.h
@@ -262,11 +262,7 @@ bool DeletePlayer(FString eosID)
 
 bool CheckToken(std::string token)
 {
-	Log::GetLog()->warn("raw token {}", token);
-
 	std::string escaped_token = HostDiscordBot::HostDiscordBotDB->escapeString(token);
-
-	Log::GetLog()->warn("escaped token {}", escaped_token);
 
 	std::string query = fmt::format("SELECT * FROM {} WHERE Token='{}'", "DiscordLinker", escaped_token);
 
